d4ddf1102f7f67415cf9c914c887d388416bb03b
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-02-21 04:40:10 +0900

    基本的なレイアウト作成

--- app/src/main/java/net/hatone/hello_grpc/HelloActivity.kt
+++ app/src/main/java/net/hatone/hello_grpc/HelloActivity.kt
@@ -3,10 +3,10 @@ package net.hatone.hello_grpc
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HelloActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hello)
     }
 }
