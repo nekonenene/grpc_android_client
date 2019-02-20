ac93f72a494264a6f10f5878c963b48dad420554
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-02-21 08:28:17 +0900

    protoをgradleでコンパイルできるように

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,16 @@
+import com.google.protobuf.gradle.*
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
+
+    id("com.google.protobuf") version "0.8.8"
 }
+
+val grpcVersion = "1.18.0"
+val protocVersion = "3.6.1"
 
 android {
     compileSdkVersion(28)
@@ -18,8 +24,11 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
+        getByName("debug") {
             isMinifyEnabled = false
+        }
+        getByName("release") {
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
@@ -35,7 +44,41 @@ dependencies {
     implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
     implementation("com.android.support:appcompat-v7:28.0.0")
     implementation("com.android.support.constraint:constraint-layout:1.1.3")
+
+    // Test
     testImplementation("junit:junit:4.12")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+
+    // gRPC
+    implementation("io.grpc:grpc-okhttp:$grpcVersion")
+    implementation("io.grpc:grpc-protobuf-lite:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protocVersion"
+    }
+
+    plugins {
+        id("javalite") {
+            artifact = "com.google.protobuf:protoc-gen-javalite:3.0.0"
+        }
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("javalite")
+                id("grpc") {
+                    option("lite")
+                }
+            }
+        }
+    }
 }
