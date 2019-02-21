08a5439426124824ce8c3eb0a9c4485f8ba86b07
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-02-21 21:04:50 +0900

    リファクタリングやエラーメッセージの変更

--- app/src/main/java/net/hatone/hello_grpc/HelloGrpcTask.kt
+++ app/src/main/java/net/hatone/hello_grpc/HelloGrpcTask.kt
@@ -1,26 +1,25 @@
 package net.hatone.hello_grpc
 
 import android.app.Activity
+import android.app.Application
 import android.os.AsyncTask
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import io.grpc.examples.hello.*
-import java.io.PrintWriter
-import java.io.StringWriter
+import kotlinx.android.synthetic.main.activity_hello.*
 import java.lang.ref.WeakReference
 import java.util.concurrent.TimeUnit
 
 class HelloGrpcTask constructor(activity: Activity) : AsyncTask<String, Void, String>() {
     private val activityReference: WeakReference<Activity> = WeakReference(activity)
+    private val app: Application = activity.application
     private var channel: ManagedChannel? = null
 
     override fun doInBackground(vararg params: String): String {
         val name = params[0]
         val age = params[1].toInt()
-        val host = "grpc-test.hatone.net/"
+        val host = "grpc-test.hatone.net"
         val port = 443
 
         return try {
@@ -28,17 +27,13 @@ class HelloGrpcTask constructor(activity: Activity) : AsyncTask<String, Void, St
             val stub = GreeterGrpc.newBlockingStub(channel)
             val request = HelloRequest.newBuilder().setName(name).setAge(age).build()
             val reply: HelloReply = stub.sayHello(request)
+
+            Log.d(javaClass.simpleName, reply.toString())
             reply.message
         } catch (e: Exception) {
             Log.e(javaClass.simpleName, "Failed to get message", e)
-
-            val sw = StringWriter()
-            val pw = PrintWriter(sw)
-            e.printStackTrace(pw)
-            pw.flush()
-            String.format("Error occurred... : %n%s", sw)
+            app.getString(R.string.error_occurred)
         }
-
     }
 
     override fun onPostExecute(result: String) {
@@ -49,9 +44,7 @@ class HelloGrpcTask constructor(activity: Activity) : AsyncTask<String, Void, St
         }
 
         val activity = activityReference.get() ?: return
-        val outputTextView: TextView = activity.findViewById(R.id.outputTextView)
-        val sendButton: Button = activity.findViewById(R.id.sendButton)
-        outputTextView.text = result
-        sendButton.isEnabled = true
+        activity.outputTextView.text = result
+        activity.sendButton.isEnabled = true
     }
 }
